ba35ca8785297c1dc42abf39f6c8f32be1e254be
runjing_liu <runjing_liu@berkeley.edu>
2020-10-19 16:05:45 -0700

    fit fastructure k = 15

--- run_structure_hgdp_data.sh
+++ run_structure_hgdp_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python structure.py -K 3 \
+python structure.py -K 15 \
       --input=./hgdp_data/huang2011_plink_files/phased_HGDP+India+Africa_2810SNPs-regions1to36 \
       --output=./fits/huang2011/phased_regions1to26 \
       --full \
